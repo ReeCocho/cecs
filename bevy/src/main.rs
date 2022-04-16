38d2c38d4e5ac16d6c2eb7da9268229fb8cf0e63
Connor <ReeCocho@gmail.com>
2022-04-16 14:35:29 -0400

    Added specs example

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -1,8 +1,7 @@
-use std::time::{Duration, Instant};
-
-use bevy_ecs::prelude::*;
+use std::{time::{Duration, Instant}, sync::Arc};
 
 use bevy_tasks::{ComputeTaskPool, TaskPoolBuilder};
+
 use cecs::{
     component::{
         filter::{Read, Write},
@@ -11,22 +10,26 @@ use cecs::{
     dispatcher::Dispatcher,
     system::{query::QueryGenerator, System},
 };
+use specs::rayon::{ThreadPool, ThreadPoolBuilder};
 
 const ITER_COUNT: usize = 500_000;
+const THREAD_COUNT: usize = 4;
 
 fn main() {
     let cecs = cecs_bench();
     let bevy = bevy_bench();
+    let specs = specs_bench();
 
     println!("cecs : {} ms", cecs.as_millis());
     println!("bevy : {} ms", bevy.as_millis());
+    println!("specs : {} ms", specs.as_millis());
 }
 
 fn cecs_bench() -> Duration {
     let mut world = cecs::world::World::new();
 
     // Create the dispatcher
-    let mut dispatcher = Dispatcher::builder().thread_count(4);
+    let mut dispatcher = Dispatcher::builder().thread_count(THREAD_COUNT);
     dispatcher.with_system(S11, &[]);
     dispatcher.with_system(S21, &[]);
     dispatcher.with_system(S31, &[]);
@@ -55,8 +58,10 @@ fn cecs_bench() -> Duration {
 }
 
 fn bevy_bench() -> Duration {
+    use bevy_ecs::prelude::*;
+
     let mut world = World::new();
-    world.insert_resource(ComputeTaskPool(TaskPoolBuilder::new().num_threads(4).build()));
+    world.insert_resource(ComputeTaskPool(TaskPoolBuilder::new().num_threads(THREAD_COUNT).build()));
 
     let mut schedule = Schedule::default();
     schedule.add_stage(
@@ -88,11 +93,76 @@ fn bevy_bench() -> Duration {
     Instant::now().duration_since(start)
 }
 
+fn specs_bench() -> Duration {
+    use specs::prelude::*;
+
+    let mut world = World::new();
+    world.register::<C0>(); 
+    world.register::<C1>(); 
+    world.register::<C2>(); 
+    world.register::<C3>(); 
+    world.register::<C4>(); 
+    world.register::<C5>(); 
+    world.register::<C6>(); 
+    world.register::<C7>(); 
+    world.register::<C8>(); 
+    world.register::<C9>(); 
+    world.register::<C10>();
+    world.register::<C11>();
+    world.register::<C12>();
+    world.register::<C13>();
+    world.register::<C14>();
+    world.register::<C15>();
+    world.register::<C16>();
+    world.register::<C17>();
+    world.register::<C18>();
+    world.register::<C19>();
+    world.register::<C20>();
+    world.register::<C21>();
+    world.register::<C22>();
+    world.register::<C23>();
+    world.register::<C24>();
+    world.register::<C25>();
+
+    let mut dispatcher = DispatcherBuilder::new()
+    .with(S11, "sys_1", &[])
+    .with(S21, "sys_2", &[])
+    .with(S22, "sys_3", &[])
+    .with(S31, "sys_4", &[])
+    .with(S32, "sys_5", &[])
+    .with(S33, "sys_6", &[])
+    .with(S41, "sys_7", &[])
+    .with(S42, "sys_8", &[])
+    .with(S43, "sys_9", &[])
+    .with(S44, "sys_10", &[])
+    .with(S51, "sys_11", &[])
+    .with(S52, "sys_12", &[])
+    .with(S53, "sys_13", &[])
+    .with(S54, "sys_14", &[])
+    .with(S55, "sys_15", &[])
+    .with_pool(Arc::new(ThreadPoolBuilder::new().num_threads(THREAD_COUNT).build().unwrap()))
+    .build();
+
+    dispatcher.setup(&mut world);
+
+    let start = Instant::now();
+
+    for _ in 0..ITER_COUNT {
+        dispatcher.dispatch(&mut world);
+    }
+
+    Instant::now().duration_since(start)
+}
+
 /// Helper macro to create components
 macro_rules! new_component {
     ($name:ident) => {
-        #[derive(Component, Copy, Clone)]
+        #[derive(bevy_ecs::prelude::Component, Copy, Clone)]
         struct $name(u32);
+
+        impl specs::prelude::Component for $name {
+            type Storage = specs::prelude::VecStorage<Self>;
+        }
 
         impl Component for $name {}
     };
@@ -104,8 +174,16 @@ macro_rules! new_system {
         struct $name;
 
         impl $name {
-            fn bevy_system(_: Query<( $(&mut $comp_w,)* $(&$comp_r,)* )>) {
+            fn bevy_system(_: bevy_ecs::prelude::Query<( $(&mut $comp_w,)* $(&$comp_r,)* )>) {
                 
+            }
+        }
+
+        impl<'a> specs::prelude::System<'a> for $name {
+            type SystemData = ( $(specs::prelude::WriteStorage<'a, $comp_w>,)* $(specs::prelude::ReadStorage<'a, $comp_r>,)* );
+
+            fn run(&mut self, _: Self::SystemData) {
+
             }
         }
 
